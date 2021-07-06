4ba01e2734d6d5beaf0dcd0d950e0ed47880b1db
DESKTOP-NFSMPDJ\fntld <myonghyoyou@gmail.com>
2021-07-06 17:54:44 +0900

    1. Query문 실행 방식 변경 (ORM 이용 방식 -> pymysql)

--- restapi_board_app/urls.py
+++ restapi_board_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from .views.notice_view import Notice
+from .views import notice_view
 
 urlpatterns = [
-    path('notice/', Notice.as_view(), name='notice')
+    path('notice/', notice_view.Notice.as_view(), name='notice'),
+    path('notice/detail', notice_view.Notice_Detail.as_view(), name='notice_detail'),
+    path('notice/write', notice_view.Notice_Write.as_view(), name='notice_write'),
+    path('notice/writeAction', notice_view.Notice_Write_Action.as_view(), name='notice_write_action'),
 ]
--- restapi_board_app/views/notice_view.py
+++ restapi_board_app/views/notice_view.py
@@ -1,25 +1,62 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from restapi_board_app.util.paging import paging
 import requests
 import json
+import datetime
 
 # Create your views here.
 class Notice(View):
     def get(self, request):
-        list_url = 'http://localhost:8000/index/notice/'
-        jsonResponse = requests.post(list_url, data={'queryString':'SELECT * FROM t_board WHERE 1=1 ORDER BY updt_dttm DESC'})
+        list_url = 'http://localhost:8000/notices/all/'
+        resDict = requests.post(list_url, data={'queryString':'SELECT * FROM t_board WHERE 1=1 ORDER BY rgst_dttm DESC'})
+        resDict = resDict.text
+        resDict_list = resDict.replace("}{","}?,/{").split("?,/")
+        tmpResDict = list()
 
-        print("-----------------------")
-        print(jsonResponse)
-        print("-----------------------")
-        print(jsonResponse.headers)
-        print("-----------------------")
-        print(jsonResponse.text)
-        print("-----------------------")
-        print(jsonResponse.text.replace("[", ""))
-        print("-----------------------")
+        for value in resDict_list:
+            tmpResDict.append(eval(value))
+
+        resDict_list = paging(request, tmpResDict)
+
+        return render(request, 'notice.html', {'resDict_list':resDict_list})
+
+class Notice_Detail(View):
+    def post(self, request):
+        list_url = 'http://localhost:8000/notices/details/'
+        queryString = 'SELECT * FROM t_board WHERE 1=1 AND brd_uid =' + request.POST.get('brd_uid')
+        resDict = requests.post(list_url, data={'queryString': queryString})
+        resDict = resDict.text
+        resDict_list = resDict.replace("}{","}?,/{").split("?,/")
+        tmpResDict = list()
+
+        for value in resDict_list:
+            tmpResDict.append(eval(value))
+
+        resDict = tmpResDict
+
+        return render(request, 'noticeDetail.html', {'resDict':resDict})
+
+class Notice_Write(View):
+    def get(self, request):
+        return render(request, 'noticeWrite.html', {})
+
+class Notice_Write_Action(View):
+    def post(self, request):
+        list_url = 'http://localhost:8000/notices/write/'
+        title = request.POST.get('title')
+        cntnt = request.POST.get('cntnt')
+        rgst_dttm = datetime.datetime.now()
         
-        for p in jsonResponse:
-            print(p.title)
+        queryString = f"INSERT INTO t_board (title, cntnt, rgst_dttm) VALUES ('{title}','{cntnt}','{rgst_dttm}')"
+        response = requests.post(list_url, data={'queryString': queryString})
 
-        return render(request, 'notice.html', {'jsonResponse':jsonResponse})
+        if(response.status_code == 200):
+            resDict = json.dumps({'resCd' : '0000', 'mem_id' : '', 'JSON' : 'Y', "resMsg" : "[삭제] 정상처리 되었습니다."})
+        else:
+            resDict = json.dumps({'resCd' : '0203', 'mem_id' : '', 'JSON' : 'Y', "resMsg" : "[삭제] 정상처리 되었습니다."})
+
+        print(resDict)
+
+        return HttpResponse(resDict)
